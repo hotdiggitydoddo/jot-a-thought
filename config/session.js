26ff7e0005377a2dfbd49058a811fe75f9c7f7f6
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2013-11-17 04:31:31 -0800

    Add session controller

--- config/session.js
+++ config/session.js
@@ -15,22 +15,22 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: 'ac2b375d971cfd9e1476f011b4d05b07'
+  secret: 'ac2b375d971cfd9e1476f011b4d05b07',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  //adapter: 'redis',
+  adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
   //
-  // host: 'grideye.redistogo.com',
-  // port: 9215,
+   host: 'grideye.redistogo.com',
+   port: 9219,
   // ttl: <redis session TTL in seconds>,
-  // db: 'redistogo',
-  // pass: '753259e366f6d59f0bd755296fa8443b'
+   db: 'redistogo',
+   pass: '348f46fe000decc5d21bc10d88834ed1'
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
